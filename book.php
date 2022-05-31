6a608d1ab39ae1aff0750e4dac21b10c18d63424
= <=>
2022-05-31 15:57:46 -0300

    actualizacion 1.5

--- book.php
+++ book.php
@@ -5,12 +5,19 @@
 include_once 'db.php';
 
 class Book extends DB{
-    
+
+    //creo una funcion donde me devuelve todos los libros
     function obtenerBooks(){
         $query = $this->connect()->query('SELECT * FROM book');
         return $query;
     }
 
-}
+    //creo una funcion donde me devuelve un libro por ID
+    function obtenerBook($id){
+        $query = $this->connect()->prepare('SELECT * FROM book WHERE id = :id');
+        $query->execute(['id' => $id]);
+        return $query;
+    }
 
+}
 ?>
