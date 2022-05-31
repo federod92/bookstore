0be1e5bff93cb3c5184c552cce045a98c99ae570
= <=>
2022-05-31 15:57:05 -0300

    actualizacion 1.51

--- apibooks.php
+++ apibooks.php
@@ -28,11 +28,58 @@ class ApiBooks{
                 array_push($books["items"], $item);
             }
         
-            echo json_encode($books);
+            //echo json_encode($books);
+            //ocupo la funcion printJASON para mostrar mi json
+            $this->printJSON($books);
+
+        }else{
+            $this->error('No hay elementos');
+        }
+    }
+
+    function getById($id){
+        $book = new Book();
+        $books = array();
+        $books["items"] = array();
+
+        $res = $book->obtenerBook($id);
+
+        if($res->rowCount() == 1){
+            $row = $res->fetch();
+        
+            $item=array(
+                    
+                    "id" => $row['id'],
+                    "isbn" => $row['isbn'],
+                    "nombre" => $row['nombre'],
+                    "autor" => $row['autor'],
+                    "precio" => $row['precio']
+                    //"imagen" => $row['imagen'],
+            );
+            array_push($books["items"], $item);
+            $this->printJSON($books);
         }else{
             echo json_encode(array('mensaje' => 'No hay elementos'));
         }
     }
+
+    //funcion para mostrar mi JSON
+
+    function printJSON($array){
+
+        echo '<code>'. json_encode($array) .'</code>';
+
+    }
+
+    //funcion para estandarizar mis mensajes de error
+    function error($mensaje){
+        echo '<code>'. json_encode(array('mensaje' => $mensaje)) .'</code>';
+        
+    }
+
+
+
+   
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php
-    
+
     //incluye y evalúa el fichero especificado durante la ejecución del script
     // Si ya se incluyó no se volverá a incluir
     include_once 'apibooks.php';
 
-   //petición de tipo GET
-    switch ($_SERVER['REQUEST_METHOD']) {
-        case 'GET':
-            $api = new ApiBooks();
-             $api->getAll();
-            break;
-        
-        default:
-            # code...
-            break;
+    $api = new ApiBooks();
+    //verificamos si el índice del arreglo está creado, si está creado lo muestra
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+
+        if(is_numeric($id)){
+            $api->getById($id);
+        }else{
+            $api->error('El id es incorrecto');
+        }
+    }else{
+        $api->getAll();
     }
-    
     
 ?>
